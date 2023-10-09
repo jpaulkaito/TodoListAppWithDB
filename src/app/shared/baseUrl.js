6dca97140cf775ed95c2797bedb52b3510e4aec4
JimPaul <jpaulkaito@gmail.com>
2023-10-09 00:58:07 -0700

    server deployed

--- src/app/shared/baseUrl.js
+++ src/app/shared/baseUrl.js
@@ -1,2 +1,2 @@
 //export const baseUrl = 'http://localhost:5000/api/todos/';
-export const baseUrl = 'https://todolistappserver-jpaulkaito.onrender.com/api/todos';
+export const baseUrl = 'https://todolistappserver-jpaulkaito.onrender.com/api/todos/';
